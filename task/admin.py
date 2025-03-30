6e206b41f891b08655714a4d405bf7626541817e
Andressa Karen <lqsandressa@gmail.com>
2025-03-30 18:06:44 -0300

    atualizando models.py e admin.py

--- task/admin.py
+++ task/admin.py
@@ -3,10 +3,18 @@ from task import models
 
 @admin.register(models.Task)
 class taskAdmin(admin.ModelAdmin):
-    list_display = 'id','title', 'status', 'deadline_date', 'description',
+    list_display = 'id','title', 'status', 'deadline_date', 'description', 'created_by', 'assigned_to',
     ordering = '-id',
     # list_filter - pra ver por status 
-    search_fields = 'title', #colocar o usuario
+    search_fields = 'title', 'created_by',
     list_per_page = 10
     list_max_show_all = 100
     list_editable = 'title', 'deadline_date', 'status',
+    
+@admin.register(models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'email',
+    ordering = 'id',
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable =  'name',
--- task/models.py
+++ task/models.py
@@ -8,6 +8,13 @@ from django.db import models
 
 # Filtros por usuário, status e data de vencimento.
 
+class CustomUser(models.Model):
+    name = models.CharField(max_length=60)
+    email = models.EmailField(max_length=254, unique=True)
+    
+    def __str__(self):
+        return f'{self.name}'
+
 class Task(models.Model):
     # ID - Primary key é criada automaticamente pelo django
     STATUS = [
@@ -17,9 +24,12 @@ class Task(models.Model):
     ]
     
     title = models.CharField(max_length=60)
-    description = models.TextField(blank=True) # blank=True -- Pode deixar sem preencher
+    description = models.TextField(blank=True)
     deadline_date = models.DateField() # N SEI SE É ASSIM
-    status = models.CharField(max_length=13, choices=STATUS, default='pendente') # N SEI SE É ASSIM
+    status = models.CharField(max_length=13, choices=STATUS, default='pendente') 
+    
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tarefas_criadas', blank=True, null=True)
+    assigned_to = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, related_name='atribuido_a', blank=True, null=True)
     
     def __str__(self):
         return f'{self.title}'
