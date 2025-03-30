5ee0e3e97cb3700e70977f98366a2b57a9086ce6
Andressa Karen <lqsandressa@gmail.com>
2025-03-30 20:46:17 -0300

    add css ja existente, atualizado a view para renderizar o arquivo index.html

--- task/views/task_views.py
+++ task/views/task_views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from task.models import Task
 
 def index(request):
+    
+    tasks = Task.objects.all()
+    
+    context = {
+        'tasks': tasks,
+    }
+    
     return render(
         request,
         'task/index.html',
+        context,
     )
